94f2187fce930148bcabca2bea561524077c3839
kurudrive <kurudrive@gmail.com>
2019-01-07 20:04:46 +0900

    複製機能追加

--- inc/duplicate-doc.php
+++ inc/duplicate-doc.php
@@ -2,16 +2,16 @@
 /*
   編集画面 _ 領収書発行のボタン追加
 /*-------------------------------------------*/
-add_action( 'post_submitbox_start', 'bvr_duplicate_staff' );
-function bvr_duplicate_staff() {
+add_action( 'post_submitbox_start', 'bvsl_duplicate_salary' );
+function bvsl_duplicate_salary() {
 	global $post;
 	$links = admin_url() . 'post-new.php?master_id=' . $post->ID;
-	if ( get_post_type() == 'others' ) {
+	if ( get_post_type() == 'salary' ) {
 	?>
 
 	<div class="duplicate-section">
 
-	<a href="<?php echo esc_url( $links ) . '&post_type=others&table_copy_type=all&duplicate_type=full'; ?>" class="button button-default button-block">この書類を複製</a>
+	<a href="<?php echo esc_url( $links ) . '&post_type=salary&table_copy_type=all&duplicate_type=full'; ?>" class="button button-default button-block">この書類を複製</a>
 
 	</div><!-- [ / #duplicate-section ] -->
 	<?php } ?>
