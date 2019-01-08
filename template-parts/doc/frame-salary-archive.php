172a5ccdbe5abe81b0d554f684f724ed8f571634
kurudrive <kurudrive@gmail.com>
2019-01-08 18:15:43 +0900

    支給分一覧での合計金額表示実装

--- template-parts/doc/frame-salary-archive.php
+++ template-parts/doc/frame-salary-archive.php
@@ -13,11 +13,80 @@ global $wp_query;
 if ( $wp_query->have_posts() ) {
 	while ( have_posts() ) :
 		the_post();
-	?>
-	<?php require( 'frame-salary.php' ); ?>
 
-<?php endwhile; ?>
-<?php } ?>
+		require( 'frame-salary.php' );
+
+		$total_earn   = $total_earn + bvsl_get_total_earn();
+		$total_pay    = $total_pay + bvsl_get_total_pay();
+		$koyou_hoken  = $koyou_hoken + bvsl_get_koyou_hoken();
+		$kenkou_hoken = $kenkou_hoken + bvsl_format_number( $post->salary_kenkou );
+		$nenkin       = $nenkin + bvsl_format_number( $post->salary_nenkin );
+		$kazeisyotoku = $kazeisyotoku + bvsl_get_kazeisyotoku();
+		$syotokuzei   = $syotokuzei + bvsl_format_number( $post->salary_syotokuzei );
+		$jyuuminzei   = $jyuuminzei + bvsl_format_number( $post->salary_jyuuminzei );
+		$koujyo       = $koujyo + bvsl_get_koujyo_total();
+		$sasihiki     = $sasihiki + bvsl_get_total_pay() - bvsl_get_koujyo_total();
+	endwhile;
+}
+?>
+
+<div class="bill-wrap">
+<div class="container">
+<div class="row">
+<div class="col-xs-6">
+	<table class="table table-bordered table-striped table-bill">
+		<tr>
+			<th>支給額</th>
+			<td class="text-right"><?php echo bvsl_format_print( $total_earn ); ?></td>
+		</tr>
+		<tr>
+			<th>総支給額（交通費込）</th>
+			<td class="text-right"><?php echo bvsl_format_print( $total_pay ); ?></td>
+		</tr>
+	</table>
+</div>
+<div class="col-xs-6">
+	<table class="table table-bordered table-striped table-bill">
+		<tr>
+			<th>雇用保険合計</th>
+			<td class="text-right"><?php echo bvsl_format_print( $koyou_hoken ); ?></td>
+		</tr>
+		<tr>
+			<th>健康保険合計</th>
+			<td class="text-right"><?php echo bvsl_format_print( $kenkou_hoken ); ?></td>
+		</tr>
+		<tr>
+			<th>厚生年金合計</th>
+			<td class="text-right"><?php echo bvsl_format_print( $nenkin ); ?></td>
+		</tr>
+		<tr>
+			<th>課税対象合計</th>
+			<td class="text-right"><?php echo bvsl_format_print( $kazeisyotoku ); ?></td>
+		</tr>
+		<tr>
+			<th>所得税合計</th>
+			<td class="text-right"><?php echo bvsl_format_print( $syotokuzei ); ?></td>
+		</tr>
+		<tr>
+			<th>住民税合計</th>
+			<td class="text-right"><?php echo bvsl_format_print( $jyuuminzei ); ?></td>
+		</tr>
+		<tr>
+			<th>控除合計</th>
+			<td class="text-right"><?php echo bvsl_format_print( $koujyo ); ?></td>
+		</tr>
+	</table>
+<table class="table table-bordered table-striped table-bill">
+	<tr>
+		<th>差引支給合計</th>
+		<td class="text-right"><?php echo bvsl_format_print( $sasihiki ); ?></td>
+	</tr>
+</table>
+</div>
+</div><!-- [ /.row ] -->
+</div><!-- [ /.container ] -->
+</div><!-- [ /.bill-wrap ] -->
+
 
 <div class="bill-no-print">
 <div class="container">
