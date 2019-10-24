2a493aa839b2235960a61010b11a12f935943e80
kurudrive <kurudrive@gmail.com>
2019-10-24 10:41:11 +0900

    [ Change version ] 0.4.2

--- bill-vektor-salary.php
+++ bill-vektor-salary.php
@@ -7,7 +7,7 @@
  * Author URI:      https://billvektor.com/
  * Text Domain:     bill-vektor-salary
  * Domain Path:     /languages
- * Version:         0.4.1
+ * Version:         0.4.2
  *
  * @package         Bill_Vektor_Salary
  */
